77d831293375dd6a9de005fd0d0b12fc0c8a44d5
simone.rainieri <simone.rainieri@gmail.com>
2019-02-12 11:00:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,8 @@ def monopoly_with_third_tier
 	               :pieces=> 4,
 	               :rent_in_dollars=> {:one_piece_owned=> 25,
 	                                   :two_pieces_owned=> 5,
-	                               :three_pieces_owned=> 100, 
+	                               :three_pieces_owned=> 100,
+	                               :four_pieces_owned=> 200,
 	               },
 	             }, 
 	             :names=> {}
